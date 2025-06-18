4a3ff66909c6605dbbb5f1f95382d207371d2242
RaimaAftab95 <raima.aftab2022@gmail.com>
2025-06-18 14:55:58 +0500

    Add .env for deployment

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,132 +1,140 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect } from "react";
 // import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
-    let cart = {};
+  let cart = {};
 
-    // all_product.length replace with 300
+  // all_product.length replace with 300
 
-    for (let index = 0; index < 300 + 1; index++) {
-        cart[index] = 0;
-    }
-    return cart;
-}
+  for (let index = 0; index < 300 + 1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
 
 const ShopContextProvider = (props) => {
+  const [all_product, setAllproduct] = useState([]);
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+  const API_BASE_URL = process.env.REACT_APP_API_URL;
 
-    const [all_product,setAllproduct] = useState([]);
-    const [cartItems, setCartItems] = useState(getDefaultCart());
+  // fetching database all product data and save it in allproduct
 
-    // fetching database all product data and save it in allproduct
+  useEffect(() => {
+    // fetch('http://localhost:4001/allproducts')
+    fetch(`${API_BASE_URL}/allproducts`)
+      .then((response) => response.json())
+      .then((data) => setAllproduct(data));
 
-    useEffect(()=>{
-        fetch('http://localhost:4001/allproducts')
-        .then((response)=>response.json())
-        .then((data)=>setAllproduct(data))
+    // using cartdata end point
+    if (localStorage.getItem("auth-token")) {
+      // fetch('http://localhost:4001/getcart',{
+      fetch(`${API_BASE_URL}/getcart`, {
+        method: "POST",
+        headers: {
+          Accept: "application/form-data",
+          "auth-token": `${localStorage.getItem("auth-token")}`,
+          "Content-Type": "application/json",
+        },
+        body: "",
+      })
+        .then((response) => response.json())
+        .then((data) => setCartItems(data));
+    }
+  }, []);
+  // loaded once when the component is mounted
+  // we are now fetching data from database insted of importing them
 
-        // using cartdata end point
-        if(localStorage.getItem('auth-token')) {
-            fetch('http://localhost:4001/getcart',{
-                 method:'POST',
-                 headers:{
-                    Accept:'application/form-data',
-                    'auth-token':`${localStorage.getItem('auth-token')}`,
-                    'Content-Type':'application/json',
-                 },
-                 body:"",
-            }).then((response)=>response.json())
-            .then((data)=>setCartItems(data));
-        }
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    // console.log(cartItems);
 
-    },[])
-// loaded once when the component is mounted
-// we are now fetching data from database insted of importing them
+    if (localStorage.getItem("auth-token")) {
+      // if we are logging we update this item id in mongo db database like if i create login id i have cartdata 1-300 ids in mongo db as i add items they will add in db
 
-    const addToCart = (itemId) => {
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-        // console.log(cartItems);
+      //   fetch("http://localhost:4001/addtocart", {
+      fetch(`${API_BASE_URL}/addtocart`, {
+        method: "POST",
+        headers: {
+          Accept: "application/form-data",
+          "auth-token": `${localStorage.getItem("auth-token")}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ itemId: itemId }),
+        // this itemId id comig from above parameter itemId
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+  };
 
-        if(localStorage.getItem('auth-token')){
-            // if we are logging we update this item id in mongo db database like if i create login id i have cartdata 1-300 ids in mongo db as i add items they will add in db
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    // adding index.js endpoint for remove cart item
+    if (localStorage.getItem("auth-token")) {
+      //   fetch("http://localhost:4001/removefromcart", {
+      fetch(`${API_BASE_URL}/removefromcart`, {
+        method: "POST",
+        headers: {
+          Accept: "application/form-data",
+          "auth-token": `${localStorage.getItem("auth-token")}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ itemId: itemId }),
+        // this itemId id comig from above parameter itemId
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+  };
 
-            fetch('http://localhost:4001/addtocart',{
-                method:'POST',
-                headers:{
-                    Accept:'application/form-data',
-                    'auth-token':`${localStorage.getItem('auth-token')}`,
-                    'Content-Type':'application/json',
-                },
-                body:JSON.stringify({"itemId":itemId})
-                // this itemId id comig from above parameter itemId
-            })
-            .then((response)=>response.json())
-            .then((data)=>console.log(data))
-        }
+  // funtion to getcart total amount
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        // passing individual product in find from all product array
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
     }
 
-    const removeFromCart = (itemId) => {
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-        // adding index.js endpoint for remove cart item
-        if(localStorage.getItem('auth-token')) {
-            fetch('http://localhost:4001/removefromcart',{
-                method:'POST',
-                headers:{
-                    Accept:'application/form-data',
-                    'auth-token':`${localStorage.getItem('auth-token')}`,
-                    'Content-Type':'application/json',
-                },
-                body:JSON.stringify({"itemId":itemId})
-                // this itemId id comig from above parameter itemId
-            })
-            .then((response)=>response.json())
-            .then((data)=>console.log(data))
-        }           
+    return totalAmount;
+  };
+
+  // function to display total cart items in nav bar
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
     }
+    return totalItem;
+  };
 
-    // funtion to getcart total amount
-    const getTotalCartAmount = () => {
-        let totalAmount = 0;
-        for (const item in cartItems) {
-            if (cartItems[item] > 0) {
-                // passing individual product in find from all product array
-                let itemInfo = all_product.find((product) => product.id === Number(item));
-                totalAmount += itemInfo.new_price * cartItems[item];
-            }
+  useEffect(() => {
+    console.log("cartItems:", cartItems);
+  }, [cartItems]); // Log cartItems whenever it changes
 
-        }
+  const contextValue = {
+    getTotalCartItems,
+    getTotalCartAmount,
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
+  // these all props in above line of code are case sensetive when call in Cartitems.jsx be carefull
 
-        return totalAmount;
-
-    }
-
-    // function to display total cart items in nav bar
-    const getTotalCartItems =  ()  =>{
-        let totalItem = 0;
-        for(const item in cartItems)
-        {
-            if(cartItems[item]>0)
-            {
-                totalItem+= cartItems[item];
-            }
-        }
-        return totalItem;
-    }
-
-
-    useEffect(() => {
-        console.log("cartItems:", cartItems);
-    }, [cartItems]); // Log cartItems whenever it changes
-
-    const contextValue = {getTotalCartItems, getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart };
-    // these all props in above line of code are case sensetive when call in Cartitems.jsx be carefull
-
-    return (
-        <ShopContext.Provider value={contextValue}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-}
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
