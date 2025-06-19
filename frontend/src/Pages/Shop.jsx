4f41ac29969e87bd78f0b48aaa0b902e94390fdb
RaimaAftab95 <raima.aftab2022@gmail.com>
2025-06-19 13:33:45 +0500

    remove shop.css

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -3,16 +3,13 @@ import Popular from "../Components/Popular/Popular";
 import Offers from "../Components/Offers/Offers";
 import NewCollections from "../Components/NewCollections/NewCollections";
 import NewsLetters from "../Components/NewsLetters/NewsLetters";
-import "./Shop.css";
 
 const Shop = () => {
   return (
     <div>
       <Hero />
-      <div className="section-wrapper">
-        <Popular />
-        <Offers />
-      </div>
+      <Popular />
+      <Offers />
       <NewCollections />
       <NewsLetters />
     </div>
