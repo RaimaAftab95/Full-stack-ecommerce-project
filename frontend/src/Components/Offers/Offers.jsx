57416694ac8324a4fe2d1691f2196c41caf1e686
RaimaAftab95 <raima.aftab2022@gmail.com>
2025-06-19 12:15:31 +0500

    update offers.css

--- frontend/src/Components/Offers/Offers.jsx
+++ frontend/src/Components/Offers/Offers.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './offers.css';
-import exclusive_image from '../Assets/exclusive_image.png';
+import React from "react";
+import "./offers.css";
+import exclusive_image from "../Assets/exclusive_image.png";
 
 const Offers = () => {
   return (
-    <div className='offers'>
+    <div className="offers">
       <div className="offers-left">
         <h1>Exclusive</h1>
         <h1>Offers For You</h1>
@@ -15,7 +15,7 @@ const Offers = () => {
         <img src={exclusive_image} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Offers
+export default Offers;
